2723c5dc49078889f8b4a2385679c30d8a91472f
Sahin <SahiCeng@DO-DAT.INT>
2018-04-05 11:59:35 +0200

    Completed VehicleSelecttion

--- identify.js
+++ identify.js
@@ -64,24 +64,6 @@ function getSubModels(vt, manufacturer, bm, restriction, locale, cb, err) {
     )
 }
 
-function getClassificationGroups(vt, manufacturer, bm, subModel, restriction, locale, cb, err) {
-    var request = {};
-    request.locale = locale;
-    request.restriction = restriction;
-    request.vehicleType = vt;
-    request.manufacturer = manufacturer;
-    request.baseModel = bm;
-    request.subModel = subModel;
-
-    datNode.callStub(
-        selectionTarget + "/getClassificationGroups",
-        request,
-        cb,
-        err
-    )
-    
-}
-
 function compileDATECode(vt, manufacturer, bm, submodel, restriction, locale, options, ctFrom, ctTo, cb, err) {
     var request = {};
     request.locale = locale;
@@ -108,6 +90,102 @@ function compileDATECode(vt, manufacturer, bm, submodel, restriction, locale, op
     )
 }
 
+function getClassificationGroups(vt, manufacturer, bm, subModel, restriction, locale, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.vehicleType = vt;
+    request.manufacturer = manufacturer;
+    request.baseModel = bm;
+    request.subModel = subModel;
+
+    datNode.callStub(
+        selectionTarget + "/getClassificationGroups",
+        request,
+        cb,
+        err
+    )
+}
+
+function getOptionsbyClassification(vt, manufacturer, bm, submodel, options, classification, ctFrom, ctTo, restriction, locale, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.vehicleType = vt;
+    request.manufacturer = manufacturer;
+    request.baseModel = bm;
+    request.subModel = submodel;
+    if (options) {
+        request.availableOptions = availableOptions // JSON-Array of options
+    }
+    request.classification = classification;
+    if (ctFrom) {
+        request.constructionTimeFrom = ctFrom;
+    }
+    if (ctTo) {
+        request.constructionTimeTo = ctTo;
+    }
+
+    datNode.callStub(
+        selectionTarget + "/getOptionsbyClassification",
+        request,
+        cb,
+        err
+    );
+}
+
+function getEngineOptions(vt, manufacturer, bm, submodel, options, restriction, locale, ctFrom, ctTo, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.vehicleType = vt;
+    request.manufacturer = manufacturer;
+    request.baseModel = bm;
+    request.subModel = submodel;
+    if (options) {
+        request.availableOptions = options // JSON-Array of options
+    }
+    if (ctFrom) {
+        request.constructionTimeFrom = ctFrom;
+    }
+    if (ctTo) {
+        request.constructionTimeTo = ctTo;
+    }
+
+    datNode.callStub(
+        selectionTarget + "/getEngineOptions",
+        request,
+        cb,
+        err
+    );
+}
+
+function getCarBodyOptions(vt, manufacturer, bm, submodel, options, restriction, locale, ctFrom, ctTo, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.vehicleType = vt;
+    request.manufacturer = manufacturer;
+    request.baseModel = bm;
+    request.subModel = submodel;
+    if (options) {
+        request.availableOptions = options // JSON-Array of options
+    }
+    if (ctFrom) {
+        request.constructionTimeFrom = ctFrom;
+    }
+    if (ctTo) {
+        request.constructionTimeTo = ctTo;
+    }
+
+    datNode.callStub(
+        selectionTarget + "/getCarBodyOptions",
+        request,
+        cb,
+        err
+    );
+}
+
 function getConstructionOptions(vt, manufacturer, bm, submodel, restriction, locale, availableOptions, ctFrom, ctTo, cb, err) {
     var request = {};
     request.locale = locale;
@@ -131,10 +209,297 @@ function getConstructionOptions(vt, manufacturer, bm, submodel, restriction, loc
         request,
         cb,
         err
-    )
+    );
 }
 
+function getConstructionPeriods(ecode, container, restriction, locale, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.datECode = ecode;
+    request.container = container;
 
+    datNode.callStub(
+        selectionTarget + "/getConstructionPeriodsN",
+        request,
+        cb,
+        err
+    );
+}
+
+function getGearingOptions(vt, manufacturer, bm, submodel, options, restriction, locale, ctFrom, ctTo, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.vehicleType = vt;
+    request.manufacturer = manufacturer;
+    request.baseModel = bm;
+    request.subModel = submodel;
+    if (options) {
+        request.availableOptions = options // JSON-Array of options
+    }
+    if (ctFrom) {
+        request.constructionTimeFrom = ctFrom;
+    }
+    if (ctTo) {
+        request.constructionTimeTo = ctTo;
+    }
+
+    datNode.callStub(
+        selectionTarget + "/getGearingOptions",
+        request,
+        cb,
+        err
+    );
+}
+
+function getDriversCabOptions(vt, manufacturer, bm, submodel, options, restriction, locale, ctFrom, ctTo, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.vehicleType = vt;
+    request.manufacturer = manufacturer;
+    request.baseModel = bm;
+    request.subModel = submodel;
+    if (options) {
+        request.availableOptions = options // JSON-Array of options
+    }
+    if (ctFrom) {
+        request.constructionTimeFrom = ctFrom;
+    }
+    if (ctTo) {
+        request.constructionTimeTo = ctTo;
+    }
+
+    datNode.callStub(
+        selectionTarget + "/getDriversCabOptions",
+        request,
+        cb,
+        err
+    );
+}
+
+function getGrossVehicleWeightOptions(vt, manufacturer, bm, submodel, options, restriction, locale, ctFrom, ctTo, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.vehicleType = vt;
+    request.manufacturer = manufacturer;
+    request.baseModel = bm;
+    request.subModel = submodel;
+    if (options) {
+        request.availableOptions = options // JSON-Array of options
+    }
+    if (ctFrom) {
+        request.constructionTimeFrom = ctFrom;
+    }
+    if (ctTo) {
+        request.constructionTimeTo = ctTo;
+    }
+
+    datNode.callStub(
+        selectionTarget + "/getGrossVehicleWeightOptions",
+        request,
+        cb,
+        err
+    );
+}
+
+function getNumberOfAxleOptions(vt, manufacturer, bm, submodel, options, restriction, locale, ctFrom, ctTo, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.vehicleType = vt;
+    request.manufacturer = manufacturer;
+    request.baseModel = bm;
+    request.subModel = submodel;
+    if (options) {
+        request.availableOptions = options // JSON-Array of options
+    }
+    if (ctFrom) {
+        request.constructionTimeFrom = ctFrom;
+    }
+    if (ctTo) {
+        request.constructionTimeTo = ctTo;
+    }
+
+    datNode.callStub(
+        selectionTarget + "/getNumberOfAxleOptions",
+        request,
+        cb,
+        err
+    );
+}
+
+function getSuspensionOptions(vt, manufacturer, bm, submodel, options, restriction, locale, ctFrom, ctTo, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.vehicleType = vt;
+    request.manufacturer = manufacturer;
+    request.baseModel = bm;
+    request.subModel = submodel;
+    if (options) {
+        request.availableOptions = options // JSON-Array of options
+    }
+    if (ctFrom) {
+        request.constructionTimeFrom = ctFrom;
+    }
+    if (ctTo) {
+        request.constructionTimeTo = ctTo;
+    }
+
+    datNode.callStub(
+        selectionTarget + "/getSuspensionOptions",
+        request,
+        cb,
+        err
+    );
+}
+
+function getTypeOfDriveOptions(vt, manufacturer, bm, submodel, options, restriction, locale, ctFrom, ctTo, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.vehicleType = vt;
+    request.manufacturer = manufacturer;
+    request.baseModel = bm;
+    request.subModel = submodel;
+    if (options) {
+        request.availableOptions = options // JSON-Array of options
+    }
+    if (ctFrom) {
+        request.constructionTimeFrom = ctFrom;
+    }
+    if (ctTo) {
+        request.constructionTimeTo = ctTo;
+    }
+
+    datNode.callStub(
+        selectionTarget + "/getTypeOfDriveOptions",
+        request,
+        cb,
+        err
+    );
+}
+
+function getWheelBaseOptions(vt, manufacturer, bm, submodel, options, restriction, locale, ctFrom, ctTo, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.vehicleType = vt;
+    request.manufacturer = manufacturer;
+    request.baseModel = bm;
+    request.subModel = submodel;
+    if (options) {
+        request.availableOptions = options // JSON-Array of options
+    }
+    if (ctFrom) {
+        request.constructionTimeFrom = ctFrom;
+    }
+    if (ctTo) {
+        request.constructionTimeTo = ctTo;
+    }
+
+    datNode.callStub(
+        selectionTarget + "/getWheelBaseOptions",
+        request,
+        cb,
+        err
+    );
+}
+
+function getEquipmentLineOptions(vt, manufacturer, bm, submodel, options, restriction, locale, ctFrom, ctTo, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.vehicleType = vt;
+    request.manufacturer = manufacturer;
+    request.baseModel = bm;
+    request.subModel = submodel;
+    if (options) {
+        request.availableOptions = options // JSON-Array of options
+    }
+    if (ctFrom) {
+        request.constructionTimeFrom = ctFrom;
+    }
+    if (ctTo) {
+        request.constructionTimeTo = ctTo;
+    }
+
+    datNode.callStub(
+        selectionTarget + "/getEquipmentLineOptions",
+        request,
+        cb,
+        err
+    );
+}
+
+function getPriceFocusCases(ecode, restriction, locale, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.datECode = ecode;
+
+    datNode.callStub(
+        selectionTarget + "/getPriceFocusCases",
+        request,
+        cb,
+        err
+    );
+}
+
+function getPriceFocusConstructionYears(datecode, restriction, locale, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.datECode = ecode;
+
+    datNode.callStub(
+        selectionTarget + "/getPriceFocusConstructionPeriods",
+        request,
+        cb,
+        err
+    );
+}
+
+function getVehicleData(datecode, container, ct, restriction, locale, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.datECode = ecode;
+    if (container) {
+        request.container = container;
+    }
+    request.constructionTime = ct;
+
+    datNode.callStub(
+        selectionTarget + "/getVehicleData",
+        request,
+        cb,
+        err
+    );
+}
+
+function getSubModelsByTextSearch(needle, restriction, locale, ctFrom, ctTo, cb, err) {
+    var request = {};
+    request.locale = locale;
+    request.restriction = restriction;
+    request.searchText = needle;
+    if (ctFrom) {
+        request.constructionTimeFrom = ctFrom;
+    }
+    if (ctTo) {
+        request.constructionTimeTo = ctTo;
+    }
+
+    datNode.callStub(
+        selectionTarget + "/getVehicleData",
+        request,
+        cb,
+        err
+    );
+}
 
 module.exports.getVehicleTypes = getVehicleTypes;
 module.exports.getManufacturer = getManufacturer;
@@ -142,4 +507,20 @@ module.exports.getBaseModels = getBaseModels;
 module.exports.getSubModels = getSubModels;
 module.exports.compileDATECode = compileDATECode;
 module.exports.getClassificationGroups = getClassificationGroups;
+module.exports.getOptionsbyClassification = getOptionsbyClassification;
+module.exports.getEngineOptions = getEngineOptions;
+module.exports.getCarBodyOptions = getCarBodyOptions;
 module.exports.getConstructionOptions = getConstructionOptions;
+module.exports.getConstructionPeriods = getConstructionPeriods;
+module.exports.getGearingOptions = getGearingOptions;
+module.exports.getDriversCabOptions = getDriversCabOptions;
+module.exports.getGrossVehicleWeightOptions = getGrossVehicleWeightOptions;
+module.exports.getNumberOfAxleOptions = getNumberOfAxleOptions;
+module.exports.getSuspensionOptions = getSuspensionOptions;
+module.exports.getTypeOfDriveOptions = getTypeOfDriveOptions;
+module.exports.getWheelBaseOptions = getWheelBaseOptions;
+module.exports.getEquipmentLineOptions = getEquipmentLineOptions;
+module.exports.getPriceFocusCases = getPriceFocusCases;
+module.exports.getPriceFocusConstructionYears = getPriceFocusConstructionYears;
+module.exports.getVehicleData = getVehicleData;
+module.exports.getSubModelsByTextSearch = getSubModelsByTextSearch;
--- index.js
+++ index.js
@@ -3,22 +3,31 @@ const config = require("./config.json");
 const identify = require("./identify.js");
 
 const locale = config.locale;
-
+/*
 identify.getVehicleTypes("ALL", locale, success, error);
 identify.getManufacturer(1, "ALL", locale, success, error);
 identify.getBaseModels(1, 130, "ALL", locale, success, error);
 identify.getSubModels(1, 130, 85, "ALL", locale, success, error);
-identify.getClassificationGroups(1, 130, 85, 1, "ALL", locale, success, error);
 identify.compileDATECode(1, 130, 85, 1, "ALL", locale, null, null, null, success, error);
-identify.getConstructionOptions(1, 40, 28, 2, "APPRAISAL", locale, [88100/*, 75205*/], null, null, success, error);
+identify.getClassificationGroups(1, 130, 85, 1, "ALL", locale, success, error);
+identify.getOptionsbyClassification(1, 130 ,85, 1, null, 11, null, null, "APPRAISAL", locale, success, error);
+identify.getEngineOptions(1, 130, 85, 1, null, "ALL", locale, null, null, success, error);
+identify.getConstructionOptions(1, 40, 28, 2, "APPRAISAL", locale, [88100, 75205], null, null, success, error);
+*/
+identify.getConstructionPeriods("015700460110001", "DE001", "REPAIR", locale, success, error);
 
 function hr() {
     console.log("----------------------------------------------------------------------")
 }
 
 function success(result, request) {
+    // The nature of the result may differ from real objects to plain string
+    // Get things straight here ...
+    if(typeof result === 'string') {
+        result = JSON.parse(result);
+    }
     console.log(JSON.stringify(request, null, 3));
-    console.log(result);
+    console.log(JSON.stringify(result, null, 3));
     hr();
 }
 
