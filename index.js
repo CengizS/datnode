51779cc2cfad6e16bfb672b4511d6af33d1fb004
CengizS <csahin@gmail.com>
2018-04-11 22:06:14 +0200

    .

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const config = require("./config.json");
 const identify = require("./identify.js");
 
 const locale = config.locale;
-/*
+
 identify.getVehicleTypes("ALL", locale, success, error);
 identify.getManufacturer(1, "ALL", locale, success, error);
 identify.getBaseModels(1, 130, "ALL", locale, success, error);
@@ -13,22 +13,21 @@ identify.getClassificationGroups(1, 130, 85, 1, "ALL", locale, success, error);
 identify.getOptionsbyClassification(1, 130 ,85, 1, null, 11, null, null, "APPRAISAL", locale, success, error);
 identify.getEngineOptions(1, 130, 85, 1, null, "ALL", locale, null, null, success, error);
 identify.getConstructionOptions(1, 40, 28, 2, "APPRAISAL", locale, [88100, 75205], null, null, success, error);
-*/
 identify.getConstructionPeriods("015700460110001", "DE001", "REPAIR", locale, success, error);
-
-function hr() {
-    console.log("----------------------------------------------------------------------")
-}
 
 function success(result, request) {
     // The nature of the result may differ from real objects to plain string
     // Get things straight here ...
     if(typeof result === 'string') {
-        result = JSON.parse(result);
+        try {
+            result = JSON.parse(result);
+        } catch(e) {
+            // result is Non-JSON - just do nothing
+        }
     }
     console.log(JSON.stringify(request, null, 3));
     console.log(JSON.stringify(result, null, 3));
-    hr();
+    console.log("----------------------------------------------------------------------")
 }
 
 function error(e) {
